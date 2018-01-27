d972a7162bcd2b0baaf507f6ccdf5ae16f35268f
mick <mick@GGJ2018.GG.local>
2018-01-27 10:54:02 +0100

    Tower + click

--- pizouille/src/Map.cpp
+++ pizouille/src/Map.cpp
@@ -4,12 +4,14 @@
 
 #include "Map.h"
 #include "res.h"
+#include "Tower.h"
 #include <iostream>
 
 Map::Map(): _pressed(false)
 {
     setResAnim(res::ui.getResAnim("map"));
     addEventListener(TouchEvent::MOVE, CLOSURE(this, &Map::onEvent));
+    addEventListener(TouchEvent::CLICK, CLOSURE(this, &Map::onEvent));
 
     _finger = new Sprite;
     _finger->setResAnim(res::ui.getResAnim("finger"));
@@ -31,4 +33,15 @@ void Map::onEvent(Event* ev)
         Vector2 center = getSize() / 2;
         _finger->setPosition(te->localPosition);
     }
+    if(te->type == TouchEvent::CLICK)
+    {
+        std::cout << "CLICK" << std::endl;
+        spTower tower = new Tower();
+        tower->attachTo(this);
+        tower->setPosition(te->localPosition);
+        tower->setVisible(true);
+
+
+    }
+
 }
