2dcc1aaf09d99f93fbf9adc4a05078dbf9d02c69
Marc Gay-Balmaz <m.gaybalmaz@gmail.com>
2018-03-29 01:02:35 +0200

    finger only appears when mouse over the area

--- pizouille/src/MonsterSelect.cpp
+++ pizouille/src/MonsterSelect.cpp
@@ -17,13 +17,15 @@ MonsterSelect::MonsterSelect(): _pressed(false)
     setResAnim(res::ui.getResAnim("carte_insecte"));
     addEventListener(TouchEvent::MOVE, CLOSURE(this, &MonsterSelect::onEvent));
     addEventListener(TouchEvent::CLICK, CLOSURE(this, &MonsterSelect::onEvent));
+    addEventListener(TouchEvent::OVER, CLOSURE(this, &MonsterSelect::onEvent));
+    addEventListener(TouchEvent::OUT, CLOSURE(this, &MonsterSelect::onEvent));
 
 
 
     _finger = new Sprite;
     _finger->setResAnim(res::ui.getResAnim("finger"));
     _finger->attachTo(this);
-    _finger->setVisible(true);
+    _finger->setVisible(false);
     _finger->setAnchor(Vector2(0.5f, 0.5f));
     _finger->setTouchEnabled(false);
 
@@ -32,13 +34,18 @@ MonsterSelect::MonsterSelect(): _pressed(false)
 void MonsterSelect::onEvent(Event* ev)
 {
 
-    TouchEvent* te = safeCast<TouchEvent*>(ev);
+    auto * te = safeCast<TouchEvent*>(ev);
 
     if (te->type == TouchEvent::MOVE)
     {
-        std::cout << "MOVE" << std::endl;
         Vector2 center = getSize() / 2;
-        //_finger->setPosition(te->localPosition);
+        _finger->setPosition(te->localPosition);
+    } else if(te->type == TouchEvent::OVER){
+
+        _finger->setVisible(true);
+    } else if(te->type == TouchEvent::OUT){
+
+        _finger->setVisible(false);
     }
     if(te->type == TouchEvent::CLICK)
     {
